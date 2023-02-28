f54ad08f18e918993dac5987cf3198544eb9c0f1
ivvnnnn <99873993+ivvnnnn@users.noreply.github.com>
2023-02-28 15:20:36 +0300

    Create buffer in certain layer

---
+++ buffer_rt.py
@@ -0,0 +1,66 @@
+interactive_mode  = 0
+#layers = ['ГТС_Line','ГТС_Poly_cliped_32643']
+#layers = ['ЛЭП_Line', 'ЛЭП_Poly_cliped_32643']
+#layers = ['Автодороги_Line', 'Антропоген_2022_лот_1_v5']
+layers = ['subhydr_utm_line', 'subhydr_utm']
+line_layer_name = layers[0]
+poly_layer_name = layers[1]
+line_layer = QgsProject.instance().mapLayersByName(line_layer_name)
+poly_layer = QgsProject.instance().mapLayersByName(poly_layer_name)
+buffer_size = 1.3
+ecs = QgsGeometry.EndCapStyle(QgsGeometry.CapFlat)
+js =  QgsGeometry.JoinStyle(QgsGeometry.JoinStyleMiter)
+
+segments_num = 7
+
+
+interactive_mode = 0
+'''
+def create_buffer():
+    features = line_layer[0].getFeatures()
+    for feat in features:
+        last_ = feat
+    line_geom = last_.geometry()
+    buffer_ = line_geom.buffer(10,0, ecs,js,0.9)
+    
+    poly_feat =  poly_layer[0].getFeature(0)
+    #poly_feat.setGeometry(buffer_)
+
+    print ('ID:', poly_feat.id())
+    fet = QgsFeature()
+    fet.setGeometry(buffer_)
+    dp = poly_layer[0].dataProvider()
+    dp.addFeatures([fet])
+    print(dp)
+    
+'''
+
+
+
+selected_features = line_layer[0].selectedFeatures()
+print(selected_features)
+
+
+try:
+    for line_feat in selected_features:
+        line_geom = line_feat.geometry()
+        buffer_ = line_geom.buffer(buffer_size,segments_num, ecs,js,0.9)
+        #print('Buffer:', buffer_)
+        poly_feat =  poly_layer[0].getFeature(0)
+        print ('ID:', poly_feat.id())
+        
+    
+        fet = QgsFeature()
+        fet.setGeometry(buffer_)
+        dp = poly_layer[0].dataProvider()
+        dp.addFeatures([fet])
+        print('DP:', dp)
+except:
+    print('Выделен объект не в слое', layers[0])
+    
+    #poly_feat =  poly_layer[0].getFeature(0)
+    #poly_feat.setGeometry(buffer_)
+
+
+
+
